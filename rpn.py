e129c0f28913b4e07b968bd917430a63bb0c29ae
umdalema <dalema@umich.edu>
2017-11-05 23:43:27 -0500

    Added coloring to the output as well as a history feature. Just type 'history' to see all the previous calculations

--- rpn.py
+++ rpn.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-
 import operator
+
+# highligting. 
+from termcolor import colored
 
 
 operators = {
@@ -11,8 +13,26 @@ operators = {
     '^': operator.pow
 }
 
+history = list()
+# green for positive numbers
+# red for negative numbers. 
+def getNumberColor(num):
+    if num > 0:
+        return 'green'
+    elif num < 0:
+        return 'red'
+    else: 
+        return 'grey'
+
+def addToHistory(arg1, arg2, token, result):
+    newHist = colored(arg1, getNumberColor(arg1)) + " " + token + " " + colored(arg2, getNumberColor(arg2))
+    newHist += " = " + colored(result, getNumberColor(result))
+
+    history.append(newHist)
+
 def calculate(myarg):
     stack = list()
+
     for token in myarg.split():
         try:
             token = int(token)
@@ -22,16 +42,24 @@ def calculate(myarg):
             arg2 = stack.pop()
             arg1 = stack.pop()
             result = function(arg1, arg2)
+            addToHistory(arg1, arg2, token, result)
             stack.append(result)
-        print(stack)
+        #print(stack)
     if len(stack) != 1:
         raise TypeError("Too many parameters")
     return stack.pop()
 
 def main():
     while True:
-        result = calculate(input("rpn calc> "))
-        print("Result: ", result)
+        inputIn = input("rpn calc> ")
+        if (inputIn == 'history'):
+            lineNum = 0
+            for line in history:
+                print(lineNum, ") ", line)
+                lineNum += 1
+        else:
+            result = calculate(inputIn)
+            print("Result: ", colored(result,getNumberColor(result)))
 
 if __name__ == '__main__':
     main()
